8fcc7500178224339ebd629cea7cbe4ebfcc506b
V.Nepochatyh <leedno@gmail.com>
2021-08-25 14:38:48 +0300

    Added language selector for conftest

--- conftest.py
+++ conftest.py
@@ -2,15 +2,19 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language with command --language")
+
 chrome_cfg = Options()
 chrome_cfg.add_argument('--disable-gpu')  # applicable to windows os only
 chrome_cfg.add_argument('start-maximized')
 
 # Инициализация браузера и сборка мусора
 @pytest.fixture
-def browser():
+def browser(request):
     print("\nStart browser for case..")
-    global browser
+    chrome_cfg.add_experimental_option('prefs', {'intl.accept_languages': request.config.getoption("browser_name")})  #Добавляем заголовок языка 
     browser = webdriver.Chrome(options=chrome_cfg, executable_path=r'C:\chromedriver\chromedriver.exe')
     yield browser
     print("\nExit browser for case..")
---
+++ test_items.py
@@ -0,0 +1,6 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
