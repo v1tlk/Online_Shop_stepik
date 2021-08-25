dbbabaf972dd4cad751fc5709237c5cb417215a9
V.Nepochatyh <leedno@gmail.com>
2021-08-25 14:56:44 +0300

    Added case

--- conftest.py
+++ conftest.py
@@ -11,10 +11,12 @@ chrome_cfg.add_argument('--disable-gpu')  # applicable to windows os only
 chrome_cfg.add_argument('start-maximized')
 
 # Инициализация браузера и сборка мусора
-@pytest.fixture
+@pytest.fixture(scope="function")
 def browser(request):
     print("\nStart browser for case..")
-    chrome_cfg.add_experimental_option('prefs', {'intl.accept_languages': request.config.getoption("browser_name")})  #Добавляем заголовок языка 
+    if request.config.getoption("language") == None:
+        raise pytest.UsageError("Choose language with command --language")
+    chrome_cfg.add_experimental_option('prefs', {'intl.accept_languages': request.config.getoption("language")})  #Добавляем заголовок языка 
     browser = webdriver.Chrome(options=chrome_cfg, executable_path=r'C:\chromedriver\chromedriver.exe')
     yield browser
     print("\nExit browser for case..")
--- test_items.py
+++ test_items.py
@@ -4,3 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
+
+class Test_BookShop:
+    def test_cart_button(self, browser):
+        browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+        assert browser.find_element_by_xpath('//*[@id="add_to_basket_form"]/button'), '"Add to cart" button not found'
